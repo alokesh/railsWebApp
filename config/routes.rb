eff7ce721faaeee5d04ffcc0ed5096e20e496678
Alokesh Das <alokesh@gmail.com>
2012-02-07 19:13:33 +0530

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ RailsWebApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-
+  match '/signup',  to: 'users#new'
   root to: 'static_pages#home'
     
   resources :microposts
