5f79b48aed7087dfa116c073744d6a579e1532c2
Alokesh Das <alokesh@gmail.com>
2012-02-08 00:03:30 +0530

    Add SSL and precompile Blueprint CSS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ RailsWebApp::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -57,4 +57,6 @@ RailsWebApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.assets.precompile += %w[ blueprint/*.css ]
 end
