bbbec0ae45046980cd216edaf8b4b288de40bdb7
Alokesh Das <alokesh@gmail.com>
2012-02-07 17:35:30 +0530

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ RailsWebApp::Application.routes.draw do
   get "users/new"
 
   get "static_pages/home"
-
+  get "static_pages/about"
   get "static_pages/help"
-
+  get "static_pages/contact"
+  
   resources :microposts
 
   resources :users
